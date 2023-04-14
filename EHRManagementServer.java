aceb8b9c5daef7ef9d2f5ad6859ff9b62987b2c2
Shaun <108523971+STRB001@users.noreply.github.com>
2023-04-14 22:25:16 +0100

    Create EHRManagementServer.java
    added employee health record server. including jmDNS registration.

---
+++ EHRManagementServer.java
@@ -0,0 +1,125 @@
+package com.example.eHealthRecords.grpc;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+public class EHRManagementServer {
+    private Server server;
+
+    private void start() throws IOException {
+        int port = 50052;
+        server = ServerBuilder.forPort(port)
+                .addService(new EHRManagementImpl())
+                .build()
+                .start();
+
+        System.out.println("EHR Management server started on port " + port);
+
+        // registering server with jmDNS
+        JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+        ServiceInfo serviceInfo = ServiceInfo.create("_grpc._tcp.local.", "EHRManagementServer", port, "path=/");
+        jmdns.registerService(serviceInfo);
+        System.out.printf("Registered service with type" + serviceInfo.getType() + " and " + serviceInfo.getName());
+
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        EHRManagementServer server = new EHRManagementServer();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    static class EHRManagementImpl extends EHRManagementGrpc.EHRManagementImplBase {
+        List<SearchPatientRecordResponse> patientRecords = new ArrayList<>();
+
+
+        @Override
+        public void searchPatientRecord(SearchPatientRecordRequest request, StreamObserver<SearchPatientRecordResponse> responseObserver) {
+            SearchPatientRecordResponse response = null;
+            for (SearchPatientRecordResponse record : patientRecords) {
+                if (record.getPatientId().equals(request.getPatientId())) {
+                    response = record;
+                    System.out.println("Searched patient record for patient ID: " + request.getPatientId() + " - Result: ");
+                    System.out.println(response.toString());
+                    break;
+                }
+            }
+
+            if (response == null) {
+                System.out.println("Patient ID " + request.getPatientId() + " was searched but no matching patient found.");
+                response = SearchPatientRecordResponse.getDefaultInstance();
+            }
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+        
+
+        @Override
+        public void updatePatientRecord(UpdatePatientRecordRequest request,
+            StreamObserver<UpdatePatientRecordResponse> responseObserver) {
+            // Don't have an actual database or anywhere to store all of the records
+            // just pretending the update is successful.
+            UpdatePatientRecordResponse response = UpdatePatientRecordResponse.newBuilder()
+                    .setSuccess(true)
+                    .setMessage("Patient record updated successfully.")
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        };
+
+        @Override
+        public StreamObserver<SharePatientRecordRequest> sharePatientRecord(
+                StreamObserver<SharePatientRecordResponse> responseObserver) {
+            return new StreamObserver<SharePatientRecordRequest>() {
+                StringBuilder receivedData = new StringBuilder();
+
+                @Override
+                public void onNext(SharePatientRecordRequest request) {
+                    receivedData.append("Part: ")
+                            .append(request.getRecordPart())
+                            .append(", Content: ")
+                            .append(request.getRecordContent())
+                            .append("\n");
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    System.err.println("Error while receiving patient record: " + t.getMessage());
+                }
+
+                @Override
+                public void onCompleted() {
+                    System.out.println("Received patient record: " + receivedData.toString());
+                    SharePatientRecordResponse response = SharePatientRecordResponse.newBuilder()
+                            .setSuccess(true)
+                            .setMessage("Patient record received and stored successfully.")
+                            .build();
+                    responseObserver.onNext(response);
+                    responseObserver.onCompleted();
+                }
+            };
+        }
+    }
+}
